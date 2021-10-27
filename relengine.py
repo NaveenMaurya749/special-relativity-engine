c542136194bfd7018f11ccdfe8bfd58ae5752b4a
NaveenMaurya749 <77260143+NaveenMaurya749@users.noreply.github.com>
2021-10-27 13:22:04 +0530

    Update relengine.py

--- relengine.py
+++ relengine.py
@@ -1,44 +1,66 @@
 c = 299792458 #speed of light in m/s
 
-class point_3d:
-public:
- _init(double, double, double)
- getCoordinates()
- setCoordinates(double x, double y, double z
- getX()
- getY()
- getZ()
+class vector_3d:
+ public:
+  _init(double, double, double)
+  getCoordinates()
+  setCoordinates(double x, double y, double z
+  double getX()
+  double getY()
+  double getZ()
+  setX(double)
+  setY(double)
+  setZ(double)
+  double magnitude()
 
-private:
- double x
- double y
- double z
+ private:
+  double x
+  double y
+  double z
 
-class point_4d:
-public:
- _init(double, double, double, double)
- getCoordinates()
- setCoordinates(double x, double y, double z, double t)
- getX()
- getY()
- getZ()
- getT()
+class vector_4d:
+ public:
+  _init(double, double, double, double)
+  getCoordinates()
+  setCoordinates(double x, double y, double z, double t)
+  getX()
+  getY()
+  getZ()
+  getT()
+  setX(double)
+  setY(double)
+  setZ(double)
+  setT(double)
 
-private:
- double x
- double y
- double z
-
+ private:
+  double x
+  double y
+  double z
 
 class body:
-public:
- _init()
- get_rest_mass()
- set_rest_mass(float)
- get_position()
- set_position(point_3d)
+ public:
+  _init()
+  get_rest_mass()
+  set_rest_mass(float)
+  get_position()
+  set_position(point_3d)
 
+ private:
+  float rest_mass = 0.0f
+  float relativistic_mass
+  vector_3d position
+  vector_3d velocity
+
+class Engine:
+ public:
+  _init()
+  run(float t)
+  getFOR()
+  setFOR(body)
+  addBody(body)
+  relativeTranslation(body)
+
+  body Bodies[]
 private:
- float rest_mass = 0.0f
- float relative_mass
- point_3d position
+  body inertialFrameOfReference
+  double time
