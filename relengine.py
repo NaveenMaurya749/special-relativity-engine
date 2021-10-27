a3f82bda1e2352429fa806fc64d1efccc91e8b30
NaveenMaurya749 <77260143+NaveenMaurya749@users.noreply.github.com>
2021-10-27 17:35:40 +0530

    Update relengine.py

--- relengine.py
+++ relengine.py
@@ -25,6 +25,33 @@ class vector_4d:
   def getCoordinates(self):
    return [self.x, self.y, self.z, self.t]
   def setCoordinates(self, x, y, z, t):
+   c = 299792458 #speed of light in m/s
+import math
+
+class vector_3d:
+  def _init_(self, x, y, z):
+   self.x = x
+   self.y = y
+   self.z = z
+
+  def getCoordinates(self):
+   return [self.x, self.y, self.z]
+  def setCoordinates(self, x, y, z):
+   self.x = x
+   self.y = y
+   self.z = z
+  def magnitude(self):
+   return math.sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
+
+class vector_4d:
+  def _init_(self, x, y, z):
+   self.x = x
+   self.y = y
+   self.z = z
+
+  def getCoordinates(self):
+   return [self.x, self.y, self.z, self.t]
+  def setCoordinates(self, x, y, z, t):
    self.x = x
    self.y = y
    self.z = z
@@ -32,33 +59,30 @@ class vector_4d:
 
 class body:
   def _init_(self):
-   pass
+   rest_mass = 0.0
+
   def _init_(self, p, v, m):
    self.rest_mass = m
    self.position = p
    self.velocity = v
 
-  get_rest_mass()
-  set_rest_mass(float)
-  get_position()
-  set_position(point_3d)
-
-  rest_mass = 0.0f
-  relativistic_mass
-  dilation_constant
-  position = new vector_3d(0.0, 0.0, 0.0)
-  velocity = new vector_4d(0.0, 0.0, 0.0)
+  def relativistic_mass(self):
+   return self.rest_mass*(1+self.velocity*self.velocity/(2*c*c))
+  def dilation_factor(self):
+   return 1/(math.sqrt(1-self.velocity*self.velocity/(c*c)))
 
 class Engine:
- public:
-  _init()
-  run(float t)
-  getFOR()
-  setFOR(body)
-  addBody(body)
-  relativeTranslation(body)
+  def _init_():
+   Bodies = []
 
-  body Bodies[]
-private:
-  body inertialFrameOfReference
-  double time
+  def run(self, t):
+   for i in Bodies:
+    i.position += i.velocity * t
+   time += t
+  def addBody(b):
+   Bodies.append(b)
+  def relativeTranslation(b):
+   for i in Bodies:
+    i.position -= b.position
+    i.position /= b.dilation_factor
+    #To Be Finished
