9877963036c75a64935a3093463c8d1e3e982966
NaveenMaurya749 <77260143+NaveenMaurya749@users.noreply.github.com>
2021-10-27 19:02:47 +0530

    Update relengine.py

--- relengine.py
+++ relengine.py
@@ -53,12 +53,17 @@ class vector_4d:
 
 class body:
   def _init_(self):
-   rest_mass = 0.0
+   self.rest_mass = 0.0
+   self.position = vector_3d(0,0,0)
+   self.velocity = vector_3d(0,0,0)
 
   def _init_(self, p, v, m):
    self.rest_mass = m
    self.position = p
    self.velocity = v
+  
+  def setForce(f):
+   self.force = f
 
   def relativistic_mass(self):
    return self.rest_mass*(1+self.velocity*self.velocity/(2*c*c))
@@ -70,9 +75,17 @@ class Engine:
    Bodies = []
 
   def run(self, t):
-   for i in Bodies:
-    i.position += i.velocity * t
-   time += t
+   j = 0
+   tps = 1000 #iterations for each second
+   while(j < t*tps):
+    dt = float(j)/tps
+    for i in Bodies:
+     i.velocity += i.force**(1/i.relativistic_mass())
+     i.position += i.velocity * dt
+    time += dt
+    j++
+
+
   def addBody(b):
    Bodies.append(b)
   def relativeTranslation(b):
