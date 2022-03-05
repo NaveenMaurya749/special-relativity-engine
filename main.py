d9aa7829461b484b7a80ef610545521fe719f616
NaveenMaurya749 <naveenmaurya749@gmail.com>
2022-03-05 13:40:22 +0530

    Comprehensive debug
    Debug to the entire system till full runtime.

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import "relengine.py" as Relengine
+import relengine as Relengine
 
 def printSummary(e):
  print("No. of bodies: ", len(e.Bodies))
+ j = 0
  for i in e.Bodies:
-  print(i, ".\nPosition: ", i.getPosition())
+  print(j, ".\nPosition: ", i.getPosition())
   print("Velocity: ", i.getVelocity())
   if(i.force != 0):
-   print("Force applied: ", i.force())
+   print("Force applied: ", i.getForce())
   print()
+  j += 1
 
 engine = Relengine.Engine()
 zeroV = Relengine.vector_3d(0, 0, 0)
-b = Relengine.body(Relengine.vector_3d(2.5e1, 4.8e3, 8.8e2), Relengine.vector_3d(1.8e2, 2.94e7, 1.83e8), 0, zeroV)
+b = Relengine.body(Relengine.vector_3d(2.5e1, 4.8e3, 8.8e2), Relengine.vector_3d(1.8e2, 2.94e7, 1.83e8), 0.1, zeroV)
 engine.addBody(b)
-engine.addBody(Relengine.body(Relengine.body(Relengine.vector_3d(0, 2, 0), Relengine.vector_3d(1.37e7, 1.3e8, 1.9e6), 5, zeroV)))
-engine.addBody(Relengine.body(Relengine.body(Relengine.vector_3d(1.39e3, 1.04e3, -3.24e10), Relengine.vector_3d(1.37e7, 2.8e4, 9.2e2), 1, zeroV)))
-engine.addBody(Relengine.body(Relengine.body(Relengine.vector_3d(1.03e2, 1.14e4, -3.35e2), Relengine.vector_3d(9.2e7, 1.5e8, 7.49e6), 500, Relengine.vector_3d(12.3, 43.6, -35.03)))) 
+#engine.addBody(Relengine.body(Relengine.body(Relengine.vector_3d(0, 2, 0), Relengine.vector_3d(1.37e7, 1.3e8, 1.9e6), 5, zeroV)))
+#engine.addBody(Relengine.body(Relengine.body(Relengine.vector_3d(1.39e3, 1.04e3, -3.24e10), Relengine.vector_3d(1.37e7, 2.8e4, 9.2e2), 1, zeroV)))
+#engine.addBody(Relengine.body(Relengine.body(Relengine.vector_3d(1.03e2, 1.14e4, -3.35e2), Relengine.vector_3d(9.2e7, 1.5e8, 7.49e6), 500, Relengine.vector_3d(12.3, 43.6, -35.03)))) 
 
 print("Before simulation and transformation: \n")
 printSummary(engine)
@@ -23,6 +25,6 @@ printSummary(engine)
 engine.run(10.0)
 engine.relativeTranslation(b)
 
-print("\n\nAfter simulation and transformation: ") 
-engine.printSummary()
+print("\n\nAfter simulation and transformation: ")
+printSummary(engine)
 
--- relengine.py
+++ relengine.py
@@ -21,14 +21,16 @@ class vector_3d:
    r.z = self.z - other.z
    return r
 
-  def __mul__(self,other):  #Dot Product
+  def __pow__(self, other):  #Dot Product
    return self.x*other.x + self.y*other.y + self.z*other.z
 
-  def __pow__(self, alpha): #Product with scalar
-   r = vector_3d(0,0,0)
-   r.x *= alpha
-   r.y *= alpha
-   r.z *= alpha
+  def __mul__(self, alpha): #Product with scalar
+   r = vector_3d(0.0,0.0,0.0)
+   if isinstance(alpha, float):
+    r.x = self.x * alpha
+    r.y = self.y * alpha
+    r.z = self.z * alpha
+    return r
    
   def getCoordinates(self):
    return [self.x, self.y, self.z]
@@ -38,11 +40,13 @@ class vector_3d:
    self.z = z
   def magnitude(self):
    return math.sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
+  def direction(self):
+   return self * (1/self.magnitude())
 
 
 class body:
   def __init__(self):
-   self.rest_mass = 0.0
+   self.rest_mass = 1.0
    self.position = vector_3d(0,0,0)
    self.velocity = vector_3d(0,0,0)
    self.force = vector_3d(0,0,0)
@@ -54,14 +58,16 @@ class body:
    self.force = f
 
   def getPosition(self):
-   return ([position.x, position.y, position.z])
+   return ([self.position.x, self.position.y, self.position.z])
   def getVelocity(self):
-   return ([velocity.x, velocity.y, velocity.z])
+   return ([self.velocity.x, self.velocity.y, self.velocity.z])
+  def getForce(self):
+   return ([self.force.x, self.force.y, self.force.z])
 
   def relativistic_mass(self):
-   return self.rest_mass*(1+self.velocity*self.velocity/(2*c*c))
+   return self.rest_mass*(1+self.velocity.magnitude()*self.velocity.magnitude()/(2*c*c))
   def dilation_factor(self):
-   return 1/(math.sqrt(1-self.velocity*self.velocity/(c*c)))
+   return 1/(math.sqrt(1-self.velocity.magnitude()*self.velocity.magnitude()/(c*c)))
 
 class Engine:
   Bodies = []
@@ -72,10 +78,11 @@ class Engine:
 
   def run(self, t):
    j = 0
+   time = 0
    tps = 1000 #iterations for each second
    while(j < t*tps):
     dt = float(j)/tps
-    for i in Bodies:
+    for i in self.Bodies:
      if(i.force != vector_3d(0, 0, 0)):
       i.velocity += (i.force*(1/i.relativistic_mass()))*dt
      i.position += i.velocity * dt
@@ -83,22 +90,22 @@ class Engine:
     j += 1
 
 
-  def addBody(b):
-   Bodies.append(b)
+  def addBody(self, b):
+   self.Bodies.append(b)
 
-  def relativeTranslation(b):
-   for i in Bodies:
+  def relativeTranslation(self, b):
+   for i in self.Bodies:
     i.position -= b.position
     
     #Lorentz Transform following
     speed = b.velocity.magnitude()
     gamma = b.dilation_factor()
-    r = b.velocity/speed
-    X = (i.position * r) #The to-be contracted length component
-    i.position = i.position - (X**(1-gamma))
+    r = b.velocity*(1/speed)
+    X = (i.position ** r) #The to-be contracted length component
+    i.position = i.position * (1.0-(X*(1-gamma)))
 
     #Now, for their speeds:
-    V_x = i.velocity * r
-    V_yz = i.velocity - V
-    V_x_prime = (V_x - b.velocity)/(1 - V_x.magnitude()*speed/(c*c))
+    V_x = r.direction() * (i.velocity ** r)
+    V_yz = i.velocity - V_x
+    V_x_prime = (V_x - b.velocity)*(1/(1 - V_x.magnitude()*speed/(c*c)))
     i.velocity = V_yz + V_x_prime
